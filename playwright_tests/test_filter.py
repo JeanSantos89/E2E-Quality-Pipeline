7ba160a3be12cc2797adca3985e04fa029fceca3
Jean Santos <jeansaantos89@gmail.com>
2025-05-01 16:54:01 -0300

    01-05

--- playwright_tests/test_filter.py
+++ playwright_tests/test_filter.py
@@ -5,12 +5,18 @@ from helpers import script_login, script_filtro
 def test_filtro(page: Page):
     script_login(page)
     script_filtro(page)
-    primeiro_item = page.wait_for_selector('(//div[@class="inventory_list"]/div)[1]')  # Ve o primeiro item da lista
+    primeiro_item = page.query_selector('(//div[@class="inventory_list"]/div)[1]')  # Ve o primeiro item da lista
     preco1 = primeiro_item.query_selector('.inventory_item_price').inner_text() # Recebe o preço do primeiro item da lista
 
-    ultimo_item = page.wait_for_selector('(//div[@class="invetory_list"]/div)[last()')   # Seleciona o ultimo item da lista
+    ultimo_item = page.query_selector('(//div[@class="inventory_list"]/div)[last()]')   # Seleciona o último item da lista
     preco2 = ultimo_item.query_selector('.inventory_item_price').inner_text() # Recebe o preço do ultimo item da lista
 
+    preco1 = float(preco1.strip('$').replace(',', ''))  # Remove símbolos e converte para float
+    preco2 = float(preco2.strip('$').replace(',', ''))  # Remove símbolos e converte para float
+
+    print(f"Preco1: {preco1}")
+    print(f"Preco2: {preco2}")
+    
     if(preco2>preco1):
         print(preco2)
         print(preco1)
